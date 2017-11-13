cca878cbf9649d06ea5d33b79912c053d395d60a
jrbarto <jrbarto@us.ibm.com>
2017-11-13 13:33:35 -0500

    Added method with too many arguments

--- src/TestChild.java
+++ src/TestChild.java
@@ -4,5 +4,9 @@ public class TestChild extends TestParent {
 	public TestChild() {
 		setName("TestChild");
 	}
+    
+    public alotOfArgs(int arg1, int arg2, int arg3, int arg4, int arg5) {
+        System.out.println("Arg sum: " + (arg1+arg2+arg3+arg4+arg5));:
+    }
 
 }
